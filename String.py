811fbab14ffb8ee39e3d100f4452f7ab7a63daa0
Harshad0729 <113705114+Harshad0729@users.noreply.github.com>
2024-12-18 14:13:18 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,19 @@
+# Creating a String
+# with single Quote
+s = 'Indian'
+print(s)
+
+# Creating a String
+# with Double Quote
+s = "I'm an Indian"
+print(s)
+
+# Creating a String
+# with triple Quote
+s = '''I'm an Indian and I live in a world of "Proud Indians"'''
+print(s)
+
+# Creating String with triple
+# Quote allows multiple lines
+s = '''Live your Life'''
+print(s)
---
+++ Traverse_string.py
@@ -0,0 +1,8 @@
+str1 = "It's difficult to survive with negativity!!!!"
+address = "Flat no 205"
+for c in str1:
+    print(c)
+
+print("Flat no 205")
+for a in address:
+    print(a)
